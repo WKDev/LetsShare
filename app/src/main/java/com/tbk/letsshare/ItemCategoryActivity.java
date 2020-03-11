6ddb498c69cc6583975cdaa0673aaec0060e80ed
WKDev <chanhyukman@gmail.com>
2020-03-11 19:48:40 +0900

    Activity_created

---
+++ app/src/main/java/com/tbk/letsshare/ItemCategoryActivity.java
@@ -0,0 +1,14 @@
+package com.tbk.letsshare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ItemCategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_category);
+    }
+}
